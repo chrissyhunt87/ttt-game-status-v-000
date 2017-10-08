664c0fbf76a7cc28d60ffc35b0edea690bd785e0
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-08 21:13:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,7 @@ def won?(board)
   WIN_COMBINATIONS.each do | win_combo |
     if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
       return win_combo
+    end
   end
   return false
 end
